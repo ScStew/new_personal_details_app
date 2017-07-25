726505c30ad720c7fd2377a8d80a6b829ca4693b
ScStew <scottmstewart2@gmail.com>
2017-07-25 18:16:17 -0400

    fixed page loop

--- app.rb
+++ app.rb
@@ -11,25 +11,26 @@ post '/name' do
 end
 
 get '/last_name' do
-	f_name = params[:first_name]
-	erb :lastname, locals: {f_name:f_name}
+first_name = params[:first_name]
+erb :last_name, locals: {f_name: first_name}
 end
 
-post '/last_n' do
+post '/l_name' do
 	f_name = params[:f_name]
 	l_name = params[:l_name]
-	redirect '/age?firstname=' + f_name + "&last_name=" + l_name
+	#redirect '/age?firstname=' + f_name + "&last_name=" + l_name
 end
 
-get '/age' do
-	f_name = params[:firstname]
-	l_name = params[:last_name]
-	erb :how_old, locals:{f_name: f_name, l_name: l_name}
-end
+# get '/age' do
+# 	f_name = params[:firstname]
+# 	l_name = params[:last_name]
+# 	"#{f_name}"
+# 	erb :how_old, locals:{f_name: f_name, l_name: l_name}
+# end
 
-post '/how_old' do
-	f_name = params[:f_name]
-	l_name = params[:l_name]
-	age = params[:age]
-	"#{age} & #{f_name} & #{l_name}"
-end
+# post '/how_old' do
+# 	f_name = params[:f_name]
+# 	l_name = params[:l_name]
+# 	age = params[:age]
+# 	"#{age} & #{f_name} & #{l_name}"
+# end
